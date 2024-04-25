4e9fd623169bd6ff2bbb3b2f8000673a345b4e97
Mateus Robers <robersmateus@gmail.com>
2024-04-25 09:50:07 -0300

    a

--- 18/cadastro.py
+++ 18/cadastro.py
@@ -1,8 +1,17 @@
-import functions
+from functions import telefone, certidao, conta
+import csv
 
-inp = int(input("Olá, como vai? Poderia inserir os valores solicitados? \n[1] Sim\n[2] Não\n---> "))
+ficha_cadastral = []
+inp = int(input("""Olá, como vai? Poderia inserir os valores solicitados?
+                \n[1] Sim\n[2] Não\n---> """))
 if inp == 1:
     telefone()
-    cpf()
+    certidao()
 else:
     print("Obrigado!")
+
+with open('catalogplay.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Price"]) # título
+    for pessoa in ficha_cadastral:
+        writer.writerow(pessoa)
--- 18/functions.py
+++ 18/functions.py
@@ -1,21 +1,57 @@
+from cadastro import ficha_cadastral
 def telefone():
     while True:
-        tel = input("Digite seu número de telefone: \n")
+        tel = input("Digite seu número de telefone: \n--->")
         if len(tel) != 11:
             print("Digite o DDD, por favor. O número de telefone deve conter 9 números.")
             continue
         else:
             print("Telefone armazenado com sucesso!")
+            tel.append(ficha_cadastral)
             break
-
-def cpf():
+    
+def certidao():
     while True:
-        cpf = int(input("Digite o número de seu CPF: \n"))
+        cpf = input("Digite o número de seu CPF: \n--->")
         if len(cpf) != 11:
             print("O número de CPF deve conter 11 digitos.")
             continue
         else:
             print("Número de CPF arquivado com sucesso!")
+            cpf.append(ficha_cadastral)
             break
 
-
+def conta():
+    while True:
+        while True:
+            agencia = input("Qual o número da sua agência bancária? \n--->")
+            if len(agencia) != 4:
+                print("O número da agência deve conter apenas 4 números.") 
+                continue
+            else:
+                print("Agência encontrada!")
+                agencia.append(ficha_cadastral)
+                break
+        
+        while True:
+            tipo_de_conta = input("Qual o tipo da sua conta?\n[1] Poupança\n[2] Conta corrente")
+            if tipo_de_conta == "1":
+                print("Conta poupança selecionada.") 
+                tipo_de_conta.append(ficha_cadastral)
+                break
+            elif tipo_de_conta == "2":
+                print("Conta corrente selecionada.") 
+                tipo_de_conta.append(ficha_cadastral)
+                break
+            else:
+                print("Selecione apenas uma das opções.")
+                continue
+        
+        num = input("Digite o número da sua conta: \n--->")
+        if len(num) != 10:
+            print("O número da conta deve conter 10 digitos. (Tente incluir o dígito / Tente excluir os '0' iniciais)")
+            continue
+        else:
+            print("Número de CPF arquivado com sucesso!")
+            break
+        
