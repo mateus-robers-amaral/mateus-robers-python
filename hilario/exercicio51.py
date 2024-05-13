2f0c54a0bbc028c95fe17554b8184c1b91a801bf
Mateus Robers <robersmateus@gmail.com>
2024-05-13 20:18:33 -0300

    adding

---
+++ hilario/exercicio51.py
@@ -0,0 +1,14 @@
+c = []
+for i in range(10):
+    a = []
+    vetor_a = int(input(f"Digite o {i+1}° número: "))
+    a.append(vetor_a)
+
+for i in range(10):
+    b = []
+    vetor_b = int(input(f"Digite o {i+1}° número: "))
+    b.append(vetor_b)
+
+if vetor_a in b and vetor_b not in a:
+    c.append(vetor_a)
+print(c)
